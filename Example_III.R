a9ac6109186c8169a408c6c534420b783cffc8ad
salgeri <57118535+salgeri@users.noreply.github.com>
2026-06-03 20:51:56 -0500

    Update Example_III.R

--- Example_III.R
+++ Example_III.R
@@ -2,7 +2,7 @@
 ################################################################################
 ##                                                                            ##
 ## Code needed to replicate the results obtained for Example III, i.e.,       ##
-## the power simualtion described in Sec 6.2 and right panel of Figure 2      ##
+## the power simulation described in Sec 6.2 and right panel of Figure 2      ##
 ##                                                                            ##
 ################################################################################
 ################################################################################
